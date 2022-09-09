b81731852e0a635d5646b3d25632fd9ca2eaaa84
Andrew <41053544+UnuntriumIIT@users.noreply.github.com>
2022-09-09 16:13:38 +0500

    е

--- tourist_site/views.py
+++ tourist_site/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+
+from tourist_site.models import Article
 
 
 def article_list(request):
-    return render(request, './tourist/article_list.html', {})
+    articles = Article.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, './tourist/article_list.html', {'articles': articles})
