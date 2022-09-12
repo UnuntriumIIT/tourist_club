de2be983c76c771714df4db8023b15066685ca1f
Andrew <41053544+UnuntriumIIT@users.noreply.github.com>
2022-09-12 10:45:16 +0500

    base template

--- tourist_site/views.py
+++ tourist_site/views.py
@@ -6,4 +6,4 @@ from tourist_site.models import Article
 
 def article_list(request):
     articles = Article.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, './tourist/article_list.html', {'articles': articles})
+    return render(request, './tourist/main_page.html', {'articles': articles})
